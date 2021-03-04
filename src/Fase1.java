574ee13d4fc2c7fd44c0a2d9f778d86d61ce9733
Strumber <mzgjar@protonmail.com>
2021-03-04 11:13:33 +0100

    Fase1(definitiu)

--- src/Fase1.java
+++ src/Fase1.java
@@ -2,8 +2,19 @@
 public class Fase1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//fase 1
+		//Crea una taula (char[]) amb el teu nom on cada posició correspongui a una lletra
+		//Fes un bucle que recorri aquesta taula i mostri per consola cadascuna de les lletres.
+		
 
+		String nom[] = {"J","O","R","D","I"};
+		
+		
+		for (String imprimir:nom) {
+			System.out.println(imprimir);
+			
+		}
+		
 	}
 
 }
