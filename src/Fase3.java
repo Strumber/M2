34702c5a3609012d2fb25e67f566a5568ca53abd
Strumber <mzgjar@protonmail.com>
2021-03-07 18:58:59 +0100

    Fase3

--- src/Fase3.java
+++ src/Fase3.java
@@ -1,46 +1,37 @@
 import java.util.*;
 
-
 import javax.swing.JOptionPane;
+
 public class Fase3 {
 
 	public static void main(String[] args) {
-		
-		HashMap<Integer,String> contenidor = new HashMap<Integer,String>();
-		//HashMap<Integer,String> vagades = new HashMap<Integer,String>();
-		int repetit =0;
-		
-		
-		String nom =JOptionPane.showInputDialog("Introdueix el teu nom:");
+
+		HashMap<Integer, String> contenidor = new HashMap<Integer, String>();
+		// HashMap<Integer,String> vagades = new HashMap<Integer,String>();
+		int repetit = 0;
+
+		String nom = JOptionPane.showInputDialog("Introdueix el teu nom:");
 		System.out.println("Nom Introduït : " + nom);
-		
-	
-		for (int p=0 ; p<nom.length(); p++) {
+
+		for (int p = 0; p < nom.length(); p++) {
 			char caracter = nom.charAt(p);
 			System.out.println(caracter);
-			
-			for (int r=0; r<nom.length(); r++) {
+
+			for (int r = 0; r < nom.length(); r++) {
 				if (nom.charAt(r) == caracter) {
-					//System.out.println ("Caracter "+ nom.charAt(r)+ " repetit");
-					//System.out.println(repetit);
-					repetit ++;
-					
+
+					repetit++;
+
 				}
-				//System.out.println(caracter + "/"+ nom.charAt(r));
+
 			}
-			
-		
-			contenidor.put(p+1,"Caracter "+nom.charAt(p)+ " Repetit "+repetit+ " Vagada/es");
-			repetit =0;
+
+			contenidor.put(p + 1, "Caracter " + nom.charAt(p) + " Repetit " + repetit + " Vagada/es");
+			repetit = 0;
 		}
-		//System.out.println(repetit);
-		
+
 		System.out.println(contenidor);
-		//System.out.println(vegades);
 
 	}
-	
-	
-	
 
 }
